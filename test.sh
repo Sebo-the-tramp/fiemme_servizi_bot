44b5d40e377f3581cc67fe6e6dac43357897d71b
Sebastian <sebastian.cavada.lab@gmail.com>
2021-11-21 17:13:48 +0100

    ✨ new stuff for terraform

---
+++ test.sh
@@ -0,0 +1,7 @@
+mkdir upload
+mkdir upload/send_message
+mkdir upload/modify_data
+cp ./code/new/modify.py ./upload/modify_data/modify.py
+cp ./code/utils/db_utils.py ./upload/modify_data/db_utils.py
+cp ./code/new/send_message.py ./upload/send_message/send_message.py
+cp ./code/utils/db_utils.py ./upload/send_message/db_utils.py
