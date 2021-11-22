125e99c0ab8478ab369c034470cd7ec8937d4b0b
Simon Gschnell <45637541+SimonGschnell@users.noreply.github.com>
2021-11-22 11:44:34 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "remote" {
+    organization = "system-engineering-lab"
+    workspaces {
+      name = "fiemme_servizi_bot"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
   profile = "default"
   region     = "eu-west-1"
