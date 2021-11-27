fd9ba3d8733e4c9a4c085ed09533698add005078
Sebastian <sebastian.cavada.lab@gmail.com>
2021-11-27 11:06:16 +0100

    🐛 modified handler

--- terraform/lambda_send_message.tf
+++ terraform/lambda_send_message.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "fiemmebot_send_reminder" {
   filename      = "../send.zip"
   function_name = "fiemmebot_send_reminder_v01"
   role          = "arn:aws:iam::148437798467:role/iam_for_lambda"
-  handler       = "modify.lambda_handler.py"
+  handler       = "send_message.lambda_handler.py"
   runtime       = "python3.9"
 
   environment {
