f463d1cc6e84c640bce3a80961b95a5e2835a792
Sebastian <sebastian.cavada.lab@gmail.com>
2021-11-27 11:40:47 +0100

    🚀 small changes to make the code fly

--- terraform/lambda_modify_data.tf
+++ terraform/lambda_modify_data.tf
@@ -3,7 +3,6 @@ resource "aws_lambda_function" "fiemmebot_modify_data" {
   function_name = "fiemmebot_modify_data_v01"
   role          = "arn:aws:iam::148437798467:role/iam_for_lambda"
   handler       = "modify.lambda_handler"
-
   runtime = "python3.9"
 
   environment {
