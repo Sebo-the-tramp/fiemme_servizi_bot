e5722052880a531d610f8c19e5cb3a2fe1d20479
Sebastian <sebastian.cavada.lab@gmail.com>
2021-11-27 16:26:23 +0100

    back_to_normal

--- terraform/lambda_layer.tf
+++ terraform/lambda_layer.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_layer_version" "telegram_bot_libraries" {
-  filename            = "python.zip"
-  layer_name          = "lambda_layer_name"
+  filename            = "../python.zip"
+  layer_name          = "telegram_bot_libraries"
   compatible_runtimes = ["python3.9"]
 }
