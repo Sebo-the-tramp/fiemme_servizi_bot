c127ceefe155c3eaca6417d4b0e00cd21f74b53f
Sebastian <sebastian.cavada.lab@gmail.com>
2021-12-14 15:15:57 +0100

    🐛 bug in pipeline

--- code/new/send_message.py
+++ code/new/send_message.py
@@ -105,7 +105,7 @@ def get_text(comune):
                 list_mondizie = list_mondizie.replace("VETRO", "")
 
         text += "🔔 Promemoria! 🔔\n\n"
-        text += "Domani pomeriggio nel comune di *" + \
+        text += "Domani mattina nel comune di *" + \
             str(DATA[comune]["nome"]) + \
             "* verrà effettuata la raccolta dei seguenti rifiuti:\n\n"
 
