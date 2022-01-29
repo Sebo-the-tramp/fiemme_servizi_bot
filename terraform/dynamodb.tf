ed7a0c3c1d6c4522dafee6b4bd9f7c3449753410
Sebastian <sebastian.cavada.lab@gmail.com>
2022-01-29 10:34:58 +0100

    small changes for terraform table definition

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "fiemme_servizi_dbv1"
+  name           = "fiemme_servizi_users"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
